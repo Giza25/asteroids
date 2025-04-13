43c372bdb212029245b065b15f5d1bcadfbcc0c9
Giza25 <dyma125@gmail.com>
2025-04-13 15:34:04 +0400

    updated .gitignore to track cache as well

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from constants import *
 
 def main():
     print(f'''
-          Starting Asteroids!
-          Screen width: {SCREEN_WIDTH}
-          Screen height: {SCREEN_HEIGHT}
-          ''')
+Starting Asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}
+''')
 
 if __name__ == "__main__":
     main()

77effed8d95731b73fbabadb5eb48254c2a44f89
Giza25 <dyma125@gmail.com>
2025-04-13 16:17:40 +0400

    Added FPS

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 from constants import *
 
-def game_loop(screen: pygame.Surface):
+def game_loop(screen: pygame.Surface, clock: pygame.time.Clock, delta: int):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        delta = clock.tick(60) / 1000
 
 def main():
     pygame.init()
@@ -18,9 +19,11 @@ Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}
 ''')
     
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    game_loop(screen)
+    game_loop(screen, clock, dt)
 
 if __name__ == "__main__":
     main()

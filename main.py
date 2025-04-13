a65be04a55854febd0e01b2a0e835328fdf18b2e
Giza25 <dyma125@gmail.com>
2025-04-13 20:19:02 +0400

    Added ability to shoot for the Player. Also made a delay on every shot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def game_loop(
         screen: pygame.Surface, 
@@ -47,6 +48,7 @@ Screen height: {SCREEN_HEIGHT}
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     clock = pygame.time.Clock()
     dt = 0
@@ -55,6 +57,7 @@ Screen height: {SCREEN_HEIGHT}
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from shot import Shot
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_DELAY
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.__clock = 1
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -14,9 +16,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    
-    def draw(self, screen: pygame.Surface):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt: int):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -25,8 +24,17 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    
     def update(self, dt: int):
         keys = pygame.key.get_pressed()
+        self.__clock += dt
+        ready_to_shoot = False
+        if self.__clock > PLAYER_SHOOT_DELAY:
+            self.__clock = 0
+            ready_to_shoot = True
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -36,4 +44,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE] and ready_to_shoot:
+            self.shoot()
 
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)

ca007428d1f7c1bbbdcaa6863a62da526ddf230a
Giza25 <dyma125@gmail.com>
2025-04-13 15:58:01 +0400

    Created a game loop. The loop displays the empty black screen

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import pygame
 from constants import *
 
+def game_loop(screen: pygame.Surface):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 def main():
+    pygame.init()
+
     print(f'''
 Starting Asteroids!
 Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}
 ''')
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_loop(screen)
 
 if __name__ == "__main__":
     main()

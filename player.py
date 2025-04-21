bee53e878d807fb96d82b1cb144cd8de6489c641
Giza25 <dyma125@gmail.com>
2025-04-21 14:34:51 +0400

    Made player always be in screen boundaries

--- player.py
+++ player.py
@@ -12,12 +12,12 @@ class Player(CircleShape):
         self.__speed = 0
         self.__clock = PLAYER_SHOOT_DELAY
     
-    def triangle(self):
+    def triangle(self, position):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = position + forward * self.radius
+        b = position - forward * self.radius - right
+        c = position - forward * self.radius + right
         return [a, b, c]
     
     def reset(self):
@@ -35,6 +35,16 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * self.__speed * dt
 
+    def __handle_out_of_bounds(self):
+        if self.position.y <= 0:
+            self.position.y = 0
+        if self.position.y >= SCREEN_HEIGHT:
+            self.position.y = SCREEN_HEIGHT
+        if self.position.x <= 0:
+            self.position.x = 0
+        if self.position.x >= SCREEN_WIDTH:
+            self.position.x = SCREEN_WIDTH
+
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
@@ -51,6 +61,7 @@ class Player(CircleShape):
         elif self.__speed < 0:
             self.__speed += self.__mass
         self.move(dt)
+        self.__handle_out_of_bounds()
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -65,4 +76,4 @@ class Player(CircleShape):
             self.shoot()
 
     def draw(self, screen: pygame.Surface):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(self.position), 2)

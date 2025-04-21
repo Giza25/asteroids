08df07222b04d4f2b5540aeef4aa966128cd5fea
Giza25 <dyma125@gmail.com>
2025-04-21 14:38:26 +0400

    Made player' shots disappear when out of the screen

--- shot.py
+++ shot.py
@@ -1,13 +1,25 @@
 import pygame
 from circleshape import CircleShape
 from constants import SHOT_RADIUS
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
 
+    def __handle_out_of_bounds(self):
+        if self.position.y <= -self.radius:
+            self.kill()
+        if self.position.y >= SCREEN_HEIGHT + self.radius:
+            self.kill()
+        if self.position.x <= -self.radius:
+            self.kill()
+        if self.position.x >= SCREEN_WIDTH + self.radius:
+            self.kill()
+
     def update(self, dt: int):
         self.position += self.velocity * dt
+        self.__handle_out_of_bounds()
 
     def draw(self, screen):
         pygame.draw.circle(screen, "purple", self.position, self.radius, 2)
